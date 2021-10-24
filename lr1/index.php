60a1c18dadb19ab3b7f65305b6a3750c42ffe300
Nikita Denisov <bananabrainb1n1n1@gmail.com>
2021-10-24 23:07:44 +0500

    who prochital, tot diego

---
+++ lr1/index.php
@@ -0,0 +1,11 @@
+<p> Дата и время:
+<p>
+<?php
+ $d=date("d.m.Y H:i");
+ echo($d);
+?>
+<ul class="menu">
+  <li><a href="http://localhost/php_denisov/WWW/lab-1-1.php">Упражнение 1-2</a></li>
+  <li><a href="http://localhost/php_denisov/WWW/lab-1-2.php">Упражнение 1-3</a></li>
+  <li><a href="http://localhost/php_denisov/WWW/lab-1-3.php">Упражнение 1-4</a></li>
+  <li><a href="http://localhost/php_denisov/WWW/lab-1-5.php">Самостоятельная работа</a></li>
